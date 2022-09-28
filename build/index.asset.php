aaa735103b78f486f7c93684ebb183a61420a898
scottcarver@gmail.com <scottcarver@gmail.com>
2022-09-28 12:00:33 -0700

    better defaults
    Signed-off-by: scottcarver@gmail.com <scottcarver@gmail.com>

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-dom-ready', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins'), 'version' => 'c3ef0fe8656093d84ffe');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-dom-ready', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins'), 'version' => 'dd3563c2259e5fddebcc');
